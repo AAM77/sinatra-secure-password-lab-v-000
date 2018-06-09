9f863df6c37ba043a04834a2de722ab45950e5a3
Mohammad Adeel <adeel.a.mhd@gmail.com>
2018-06-09 17:31:42 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -54,10 +54,10 @@ class ApplicationController < Sinatra::Base
   patch "/account" do
     @user = current_user
     if params[:depost]
-      @user.balance += params[:deposit].to_f
+      @user.balance.to_f += params[:deposit].to_f
       @user.save
     elsif params[:withdraw]
-      @user.balance -= params[:withdraw].to_f
+      @user.balance.to_f -= params[:withdraw].to_f
       @user.save
     else
       @user.balance = @user.balance
